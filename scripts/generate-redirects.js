09022ec5d789012b13e80c5287bc6470382fc764
Mateus Vallim <mateus.vallim@gmail.com>
2025-05-19 17:57:04 -0300

    Implement redirect generation and Netlify CMS configuration

---
+++ scripts/generate-redirects.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const redirectsDir = path.join(__dirname, '../content/redirects');
+const outputFile = path.join(__dirname, '../_redirects');
+
+let redirects = '';
+
+fs.readdirSync(redirectsDir).forEach(file => {
+  const fileContent = fs.readFileSync(path.join(redirectsDir, file), 'utf-8');
+  const from = fileContent.match(/from: (.+)/)?.[1];
+  const to = fileContent.match(/to: (.+)/)?.[1];
+  const status = fileContent.match(/status: (\d+)/)?.[1] || '301';
+
+  if (from && to) {
+    redirects += `${from} ${to} ${status}\n`;
+  }
+});
+
+fs.writeFileSync(outputFile, redirects);
+console.log('Arquivo de redirects gerado com sucesso!');
